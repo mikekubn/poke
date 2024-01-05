60f0d945c8925a883cccbcb2653dcac73a900bc6
Michael Kubín <michael.kubn@gmail.com>
2024-01-05 11:31:01 +0100

    fix: eslint setup

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,9 +1,9 @@
-import Image from 'next/image';
+import React from 'react';
 
-export default function Home() {
+const Home = (): React.ReactElement => {
   return (
-    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-      HomePage
-    </main>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">HomePage</main>
   );
-}
+};
+
+export default Home;
